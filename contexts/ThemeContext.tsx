a4baff8d506406760afe5476209e82af93ac2d42
Denis Zaratan Pasin <zaratan@hey.com>
2020-11-08 10:02:26 +0900

    Keep theme pref in LS

--- contexts/ThemeContext.tsx
+++ contexts/ThemeContext.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
-import React, { createContext, useState, ReactNode } from 'react';
+import React, { createContext, useState, ReactNode, useEffect } from 'react';
 
 type ContextType = {
   toggleDark: () => void;
@@ -16,8 +16,18 @@ const defaultContext: ContextType = {
 const ThemeContext = createContext(defaultContext);
 export const ThemeContextProvider = ({ children }: { children: ReactNode }) => {
   const [isDark, setIsDark] = useState(true);
+
+  useEffect(() => {
+    const lsDark = JSON.parse(localStorage.getItem('ThemeContext:isDark'));
+    console.log({ lsDark });
+    if (lsDark !== undefined) {
+      setIsDark(lsDark);
+    }
+  }, []);
+
   const context: ContextType = {
     toggleDark: () => {
+      localStorage.setItem('ThemeContext:isDark', String(!isDark));
       setIsDark(!isDark);
     },
     isDark,

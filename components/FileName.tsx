2b2ac9955b36dce43f4cba519bd360e53c13b43a
Denis Zaratan Pasin <zaratan@hey.com>
2020-11-10 10:23:37 +0900

    shmol progress

--- components/FileName.tsx
+++ components/FileName.tsx
@@ -2,8 +2,32 @@ import { setMaxListeners } from 'process';
 import React from 'react';
 import styles from './FileName.module.scss';
 
-const FileName = ({ filename }: { filename: string }) => (
-  <span className={styles.fileName}>{filename}</span>
-);
+const FileName = ({
+  filename,
+  line,
+  blogGithub,
+}: {
+  filename: string;
+  line?: string;
+  blogGithub?: boolean;
+}) => {
+  if (blogGithub) {
+    return (
+      <a
+        className={styles.fileName}
+        href={`https://github.com/zaratan/zarablog-next/blob/main/${filename}${
+          line ? `#L${line}` : ''
+        }`}
+      >
+        {filename}:{line}
+      </a>
+    );
+  }
+  return (
+    <span className={styles.fileName}>
+      {filename}:{line}
+    </span>
+  );
+};
 
 export default FileName;

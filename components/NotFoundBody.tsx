cbe7c238fb67205d5037bfe7923c62b4a7f6424e
Denis Zaratan Pasin <zaratan@hey.com>
2021-03-24 17:17:25 +0900

    End of 404

--- components/NotFoundBody.tsx
+++ components/NotFoundBody.tsx
@@ -2,6 +2,11 @@ import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import classes from './NotFoundBody.module.scss';
 
+const LANG_MAPPING = {
+  fr: 'Français',
+  en: 'English',
+};
+
 const NotFoundBody = ({ pathsLocales }: { pathsLocales: any }) => {
   const [otherLangExist, setOtherLangExist] = useState([]);
   const [currentPath, setCurrentPath] = useState('');
@@ -14,14 +19,35 @@ const NotFoundBody = ({ pathsLocales }: { pathsLocales: any }) => {
 
   return (
     <div className={classes.container}>
-      <h1>This page doesn't exist.</h1>
+      <h1>This page doesn't exist</h1>
+      <p>
+        Your article might not have been translated yet. If you would like to
+        request a translation, please add an issue here:{' '}
+        <a
+          href="https://github.com/zaratan/zarablog-next/issues"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github Issue
+        </a>
+        .
+      </p>
       {otherLangExist.length === 0 ? null : (
         <p>
-          But it exists in other languages:{' '}
-          {otherLangExist.map((otherLang) => (
-            <Link href={currentPath} locale={otherLang}>
-              <a>{otherLang}</a>
-            </Link>
+          However this article already exists in{' '}
+          {otherLangExist.length === 1 ? 'an' : ''}other language
+          {otherLangExist.length === 1 ? '' : 's'}:{' '}
+          {otherLangExist.map((otherLang, i) => (
+            <>
+              <Link
+                href={currentPath}
+                key={`${currentPath}-${otherLang}`}
+                locale={otherLang}
+              >
+                <a>{LANG_MAPPING[otherLang]}</a>
+              </Link>
+              {i === otherLangExist.length - 1 ? '.' : ', '}
+            </>
           ))}
         </p>
       )}
